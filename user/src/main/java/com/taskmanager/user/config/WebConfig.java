a63b9b5a5ff6116d429569ccf72b7e021982494a
tech_learner <asrdeepak2811@gmail.com>
2024-06-16 17:07:28 +0530

    Refactored Code

--- user/src/main/java/com/taskmanager/user/config/WebConfig.java
+++ user/src/main/java/com/taskmanager/user/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Replace with your frontend URL
+                .allowedOrigins("*") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
