ed44e06b3c56aa0fd090379acc9e41bb2b76d797
tech_learner <asrdeepak2811@gmail.com>
2024-06-16 10:50:31 +0530

    Initial Commit

--- my-task-manager/src/routes/tasks/updateTask/[id]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import TaskForm from '../../../components/TaskForm.svelte';
-    import { page } from '$app/stores';
-    console.log(page.params)
-    </script>
-    <TaskForm />
